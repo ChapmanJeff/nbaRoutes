80637b64ef450cd858b590961eb8137359f440f3
Jeff Chapman <jeffcchapman@gmail.com>
2015-01-27 09:45:58 -0700

    finished through 5

--- js/app.js
+++ js/app.js
@@ -4,4 +4,28 @@ app.config(function($routeProvider, $httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
 
   //router here
+  $routeProvider
+  	.when('/', {
+  		templateUrl: 'js/home/homeTmpl.html',
+  		controller: 'homeCtrl',
+  		resolve: {
+  			allData: function (teamService, $route) {
+  				var newData = {};
+  				newData.lakers = teamService.getTeamData('losangeleslakers');
+  				newData.jazz = teamService.getTeamData('utahjazz');
+  				newData.miami = teamService.getTeamData('miamiheat');
+  				return newData;
+  			}
+  		}
+  	})
+  	.when('/teams/:team', {
+  		templateUrl: 'js/teams/teamTmpl.html',
+  		controller: 'teamCtrl',
+  		resolve: {
+  			teamData: function(teamService, $route) {
+  				return teamService.getTeamData($route.current.params.team);
+  			}
+  		}
+  	})
+  	.otherwise('/')
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,44 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+	this.addNewGame = function (gameObj) {
+		var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+		if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore)) {
+			gameObj.won = true;
+		} else {
+			gameObj.won = false;
+		}
+		return $http ({
+			method: "POST",
+			url: url,
+			data: gameObj
+		})
+	}
+
+	this.getTeamData = function (team) {
+		var deferred = $q.defer();
+		var url = 'https://api.parse.com/1/classes/' + team;
+		$http ({
+			method: 'GET',
+			url: url,
+		}).then(function(res){
+			var results = res.data.results;
+			var wins = 0;
+			var losses = 0;
+			for (var i = 0; i < results.length; i++) {
+				if (results[i].won === true) {
+					wins++;
+				} else {
+					losses++;
+				}
+			}
+			// results.push({wins: wins, losses: losses});
+			results.wins = wins;
+			results.losses = losses;
+			deferred.resolve(results)
+		})
+
+		return deferred.promise
+	}
+
 });
